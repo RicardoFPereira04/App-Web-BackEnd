1479f65a1621781a6b599fbfe420c847f06b9e5d
Ricardo F Pereira <ricardofonseca_1@hotmail.com>
2023-04-26 09:14:54 -0300

    Add project files.

---
+++ Models/Veiculo.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace app_web_backend.Models
+{
+	[Table("Veículos")]
+	public class Veiculo
+	{
+		[Key]
+        public int Id { get; set; }
+
+		[Required(ErrorMessage ="Obrigatório informar o nome!")]
+        public string Nome { get; set; }
+
+		[Required(ErrorMessage = "Obrigatório informar a placa!")]
+		public string Placa { get; set;}
+    }
+}
